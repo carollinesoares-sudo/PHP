1ff036a498d3eb468bc741e08f5433a74db574c1
ccarolmaias <carolline.soares@aluno.senai.br>
2026-04-28 14:55:22 -0300

    php

--- index.php
+++ index.php
@@ -1,41 +1,20 @@
 <?php
-$nomeEmpresaA = "php Solutions";
-$faturamentoA = 50000;
-$despesasA = 32000;
-$funcionariosA = 12;
 
+$nomeEmpresa = "php Solutions";
+$faturamento = 50000;
+$despesas = 32000;
+$funcionarios = 12;
 
-$nomeEmpresaB = "Info Digital";
-$faturamentoB = 65000;
-$despesasB = 40000;
-$funcionariosB = 15;
+// calculo simples
+$lucro = $faturamento - $despesas;
+$media = $faturamento / $funcionarios;
 
+echo "Relatorio da empresa <br><br>";
 
-$lucroA = $faturamentoA - $despesasA;
-$lucroB = $faturamentoB - $despesasB;
-
-
-$mediaA = $faturamentoA / $funcionariosA;
-$mediaB = $faturamentoB / $funcionariosB;
-
-
-if ($lucroA > $lucroB) {
-    $resultado = $nomeEmpresaA . " tem maior lucro que " . $nomeEmpresaB;
-} else {
-    $resultado = $nomeEmpresaB . " tem maior lucro que " . $nomeEmpresaA;
-}
-
-echo "Relatorio das empresas <br><br>";
-
-echo "Empresa A: " . $nomeEmpresaA . "<br>";
-echo "Lucro: " . $lucroA . "<br>";
-echo "Media por funcionario: " . $mediaA . "<br><br>";
-
-echo "Empresa B: " . $nomeEmpresaB . "<br>";
-echo "Lucro: " . $lucroB . "<br>";
-echo "Media por funcionario: " . $mediaB . "<br><br>";
-
-echo "Resultado: <br>";
-echo $resultado;
+echo "Empresa: " . $nomeEmpresa . "<br>";
+echo "Faturamento: " . $faturamento . "<br>";
+echo "Despesas: " . $despesas . "<br>";
+echo "Lucro: " . $lucro . "<br>";
+echo "Media por funcionario: " . $media . "<br>";
 
 ?>
