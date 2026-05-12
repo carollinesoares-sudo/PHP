a9affbbb28522252af35388bd82019c9fa180fe0
ccarolmaias <carolline.soares@aluno.senai.br>
2026-05-12 08:05:52 -0300

    12/05/2026

--- att8.php
+++ att8.php
@@ -1,18 +1,18 @@
 <?php
-function calcularSalario($salario, $bonus) {
-    return $salario + $bonus;
+function calcularSalario($salario, $bonus) {// Função para calcular o salário final
+    return $salario + $bonus;// Retorna a soma do salário e do bônus
 }
 
-function exibir($nome, $salarioFinal) {
-    echo "Nome: $nome <br>";
-    echo "Salário: R$ $salarioFinal <br><br>";
+function exibir($nome, $salarioFinal) {// Função para exibir o nome e o salário final
+    echo "Nome: $nome <br>";// Exibe o nome
+    echo "Salário: R$ $salarioFinal <br><br>";// Exibe o salário final
 }
 
 
-$valor = calcularSalario(2000, 200);
-exibir("Carol", $valor);
+$valor = calcularSalario(2000, 200);// Calcula o salário final para Carol
+exibir("Carol", $valor);// Exibe o nome e o salário final de Carol
 
-$valor = calcularSalario(1500, 100);
+$valor = calcularSalario(1500, 100);// Calcula o salário final para Evelyn
 exibir("Evelyn", $valor);
 
 ?>
